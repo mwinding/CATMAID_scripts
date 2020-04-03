427eab427e6a454b1dba463193e6ea24feafc34f
Michael Winding <mwinding@alumni.nd.edu>
2020-04-03 17:15:55 +0100

    issues with pymaid

--- annotations/reannotating_CNs.py
+++ annotations/reannotating_CNs.py
@@ -1,12 +1,15 @@
 import sys
-sys.path.append("/Volumes/Google Drive/My Drive/python_code/CATMAID_scripts/")
+sys.path.append("/Volumes/GoogleDrive/My Drive/python_code/CATMAID_scripts/")
 
 import pymaid
 import pyoctree
 from pymaid_creds import url, name, password, token
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import pandas as pd
 
-#pymaid.clear_cache()
 rm = pymaid.CatmaidInstance(url, name, password, token)
 
-annotated = pymaid.get_annotated('MB nomenclature')
-print(annotated)
+#annotated = pymaid.get_annotated('MB nomenclature')
+#print(annotated)
